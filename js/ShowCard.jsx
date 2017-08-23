9716ff26a12c9724e1de20115da8b3eb91be7eb0
Aidan Bane <aidanfbane@gmail.com>
2017-08-22 20:06:06 -0700

    (feat) Added proptypes to ShowCard

--- js/ShowCard.jsx
+++ js/ShowCard.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { shape, string } from 'prop-types';
 
 const ShowCard = props =>
   <div className="show-card">
@@ -16,5 +17,14 @@ const ShowCard = props =>
       {props.show.description}
     </p>
   </div>;
+
+ShowCard.propTypes = {
+  show: shape({
+    poster: string.isRequired,
+    title: string.isRequired,
+    year: string.isRequired,
+    description: string.isRequired
+  }).isRequired
+};
 
 export default ShowCard;
