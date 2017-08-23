1de342d7854efc0fd5b49905d4534fa27e81af6c
Aidan Bane <aidanfbane@gmail.com>
2017-08-22 21:04:30 -0700

    (feat) Added filter to Search component

--- js/Search.jsx
+++ js/Search.jsx
@@ -1,12 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
 import preload from '../data.json';
 import ShowCard from './ShowCard';
 
-const Search = () =>
-  <div className="search">
-    <div>
-      {preload.shows.map(show => <ShowCard key={show.imdbID} show={show} />)}
-    </div>
-  </div>;
+class Search extends Component {
+  state = {
+    searchTerm: ''
+  };
+  handleSearchTermChange = e => {
+    this.setState({ searchTerm: e.target.value });
+  };
+  render() {
+    return (
+      <div className="search">
+        <div>
+          <header>
+            <h1>Bane-video</h1>
+            <input
+              onChange={this.handleSearchTermChange}
+              value={this.state.searchTerm}
+              type="text"
+              placeholder="Search here..."
+            />
+          </header>
+          {preload.shows
+            .filter(show =>
+              `${show.title} ${show.description} ${show.year}`
+                .toUpperCase()
+                .includes(this.state.searchTerm.toUpperCase().trim())
+            )
+            .map(show => <ShowCard key={show.imdbID} show={show} />)}
+        </div>
+      </div>
+    );
+  }
+}
 
 export default Search;
