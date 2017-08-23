2b3edd86decdcd2c13d08f482ea5b46f0f406964
Aidan Bane <aidanfbane@gmail.com>
2017-08-22 19:18:10 -0700

    (feat) Implemented HashRouter

--- js/ClientApp.jsx
+++ js/ClientApp.jsx
@@ -1,19 +1,12 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { HashRouter, Route } from 'react-router-dom';
+import Landing from './Landing';
 
-const MyTitle = props =>
-  <div>
-    <h1 style={{ color: props.color }}>
-      {props.title}
-    </h1>
-  </div>;
-
-const MyFirstComponent = () =>
-  <div id="my-first-component">
-    <MyTitle title="Game of Throne" color="red" />
-    <MyTitle title="House of Cards" color="YellowGreen" />
-    <MyTitle title="The Office" color="Peru" />
-    <MyTitle title="Rick and Morty" color="blue" />
-  </div>;
-
-render(React.createElement(MyFirstComponent), document.getElementById('app'));
+const App = () =>
+  <HashRouter>
+    <div className="app">
+      <Route exact path="/" component={Landing} />
+    </div>
+  </HashRouter>;
+render(<App />, document.getElementById('app'));
---
+++ js/Landing.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Landing = () =>
+  (<div className="landing">
+    <h1>Bane-video</h1>
+    <a>or Browse All</a>
+  </div>);
+
+export default Landing;
